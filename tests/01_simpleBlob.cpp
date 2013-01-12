938abed8bd598fec4632db6648c2efdbee958ac4
Miłosz Kosobucki <milosz@kosobucki.pl>
2013-01-12 15:38:04 +0100

    Added more steps to the 01_simpleBlob testcase.

--- tests/01_simpleBlob.cpp
+++ tests/01_simpleBlob.cpp
@@ -2,26 +2,43 @@
 #include "context.h"
 #include "grid.h"
 #include "blob.h"
+#include "marchingcubes.h"
+#include "util.h"
 
 #include <iostream>
+#include <CL/cl.hpp>
+
+using namespace std;
 
 int main()
 {
-	Context ctx;
-	cl::CommandQueue q = ctx.getQueues()[0];
-	Grid grid(
-		uint3(64,64,64),
-		float3(5.0f/64.0f, 5.0f / 64.0f, 5.0f / 64.0f),
-		float3(-5.0f, -5.0f, -5.0f),
-		ctx.getClContext(),
-		ctx.getQueues()[0]
-	);
+	try {
+		Context ctx;
+		cl::CommandQueue q = ctx.getQueues()[0];
+		Grid grid(
+			uint3(64,64,64),
+			float3(5.0f/64.0f, 5.0f / 64.0f, 5.0f / 64.0f),
+			float3(-5.0f, -5.0f, -5.0f),
+			ctx.getClContext(),
+			ctx.getQueues()[0]
+		);
+		
+		float3 blobs[] = { {0.0f, 0.0f, 0.0f, 3.0f} };
+		
+		Blob* blobProgram = ctx.getBlobProgram();
 	
-	float3 blobs[] = { {0.0f, 0.0f, 0.0f, 3.0f} };
-	
-	Blob* blobProgram = ctx.getBlobProgram();
-	
-	blobProgram->runBlob(blobs, 1, grid);
+		blobProgram->runBlob(blobs, 1, grid);
+		
+		MarchingCubes* mc = ctx.getMcProgram();
+		mc->compute(grid, 1.0f);
+	} catch ( cl::Error &e ) {
+		cerr
+		  << "OpenCL runtime error at function " << endl
+		  << e.what() << endl
+		  << "Error code: "<< endl
+		  << errorString(e.err()) << endl;
+		return 1;
+	}
 	
 	return 0;
 }
