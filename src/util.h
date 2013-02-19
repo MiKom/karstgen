f165d427c70a99c1d16ebc9e6803a91f53789645
Miłosz Kosobucki <milosz@kosobucki.pl>
2013-02-19 22:42:42 +0100

    Added buffer dumping helper functions to utils.

--- src/util.h
+++ src/util.h
@@ -1,10 +1,15 @@
 #ifndef __KARSTGEN_UTIL_H__
 #define __KARSTGEN_UTIL_H__
 
-#include<cstdio>
+#include "mathtypes.h"
 
-#include<string>
-#include<iostream>
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <memory>
+
+#include <CL/cl.hpp>
+
 /** \file */
 
 /**
@@ -83,6 +88,120 @@ void run1DKernelMultipleQueues(
 	const std::vector<cl::Event>* events = NULL
 );
 
+/**
+  Dump 1D buffer to output stream.
+  
+  Each element will be printed with respective operator<< operator. Elements
+  will be divided by commas. Newline will be printed at the end of the output.
+  
+  \param os output stream that will be used to dump the buffer.
+  \param buffer buffer to be dumped
+  \param size number of elements in the buffer. Each should be of size sizeof(T)
+  \param queue OpenCL command queue that will be used to read the contents of te buffer.
+  Buffer should be valid in this queue.
+  */
+template<class T>
+void dump1DBuffer(std::ostream& os, cl::Buffer& buffer, size_t size, cl::CommandQueue& queue)
+{
+	std::unique_ptr<T[]> hostBuf{new T[size]};
+	queue.enqueueReadBuffer(
+		buffer,
+		CL_TRUE,
+		0,
+		sizeof(T) * size,
+		hostBuf.get()
+	);
+	for(int i=0; i<size; i++) {
+		os << hostBuf.get()[i] << ", ";
+	}
+	os << std::endl;
+}
+
+/**
+  Dump 2D buffer to output stream.
+  
+  Each element will be printed with respective operator<< operator. Elements in
+  a row will be divided by commas. Each row will be in separate line.
+  Newline will be printed at the end of the output.
+  
+  \param os output stream that will be used to dump the buffer.
+  \param buffer OpenCL buffer to be dumped
+  \param sizeX number of columns in the buffer ("width" of the table)
+  \param sizeX number of rows in the buffer ("height" of the table)
+  \param queue OpenCL command queue that will be used to read the contents of te buffer.
+  Buffer should be valid in this queue.
+  */
+template<class T>
+void dump2DBuffer(
+	std::ostream& os,
+	cl::Buffer& buffer,
+	size_t sizeX,
+	size_t sizeY,
+	cl::CommandQueue& queue
+)
+{
+	std::unique_ptr<T[]> hostBuf{new T[sizeX*sizeY]};
+	queue.enqueueReadBuffer(
+		buffer,
+		CL_TRUE,
+		0,
+		sizeof(T) * sizeX * sizeY,
+		hostBuf.get()
+	);
+	for(int i=0; i<sizeY; i++) {
+		for(int j=0; j<sizeX; j++) {
+			os << hostBuf.get()[i*sizeX + j] << ", ";
+		}
+		os << std::endl;
+	}
+	
+}
+
+/**
+  Dump 3D buffer to output stream.
+  
+  Each element will be printed with respective operator<< operator. Elements in
+  a row will be divided by commas. Each row will be in separate line. 2D "slices"
+  or tables of the buffer will be separated by empty line
+  Newline will be printed at the end of the output.
+  
+  \param os output stream that will be used to dump the buffer.
+  \param buffer OpenCL buffer to be dumped
+  \param sizeX size of the buffer in the x plane ("width" of the grid)
+  \param sizeY size of the buffer in the y plane ("height" of the grid)
+  \param sizeZ size of the buffer in the z plane ("depth" of the table)
+  \param queue OpenCL command queue that will be used to read the contents of te buffer.
+  Buffer should be valid in this queue.
+  */
+template<class T>
+void dump3DBuffer(
+	std::ostream& os,
+	cl::Buffer& buffer,
+	size_t sizeX,
+	size_t sizeY,
+	size_t sizeZ,
+	cl::CommandQueue& queue
+)
+{
+	std::unique_ptr<T[]> hostBuf{new T[sizeX*sizeY*sizeZ]};
+	queue.enqueueReadBuffer(
+		buffer,
+		CL_TRUE,
+		0,
+		sizeof(T) * sizeX * sizeY * sizeZ,
+		hostBuf.get()
+	);
+	for(int i=0; i<sizeZ; i++) {
+		for(int j=0; j<sizeY; j++) {
+			for(int k=0; k<sizeX; k++) {
+				os << hostBuf.get()[i*sizeX*sizeY + j*sizeX + k] << ", ";
+			}
+			os << std::endl;
+		}
+		os << std::endl;
+	}
+	
+}
 
 /**
   Run 1D kernel on single command queue.
