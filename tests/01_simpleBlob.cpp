4415442f0114a3e87d76ecb139f7e884c67f59d0
Miłosz Kosobucki <milosz@kosobucki.pl>
2013-04-11 22:34:48 +0900

    SimpleBlob test now renders two blobs and outputs avr file with mesh.

--- tests/01_simpleBlob.cpp
+++ tests/01_simpleBlob.cpp
@@ -7,30 +7,66 @@
 
 #include <iostream>
 #include <CL/cl.hpp>
+#include <avr/avr++.h>
 
 using namespace std;
+using namespace AVR;
 
 int main()
 {
 	try {
-		Context ctx;
+		Context ctx{true};
 		cl::CommandQueue q = ctx.getQueues()[0];
 		Grid grid(
 			uint3(64,64,64),
-			float3(5.0f/64.0f, 5.0f / 64.0f, 5.0f / 64.0f),
+			float3(15.0f/64.0f, 15.0f / 64.0f, 15.0f / 64.0f),
 			float3(-2.5f, -2.5f, -2.5f),
 			ctx.getClContext(),
 			ctx.getQueues()[0]
 		);
 		
-		float3 blobs[] = { {0.0f, 0.0f, 0.0f, 3.0f} };
+		float3 blobs[] = { {0.0f, 0.0f, 0.0f, 1.94f}, {0.0f, 0.5f, 2.0f, 2.0f} };
 		
 		Blob* blobProgram = ctx.getBlobProgram();
 	
-		blobProgram->runBlob(blobs, 1, grid);
+		blobProgram->runBlob(blobs, sizeof(blobs) / sizeof(blobs[0]), grid);
+		
 		
 		MarchingCubes* mc = ctx.getMcProgram();
-		mc->compute(grid, 1.0f);
+		MCMesh mcmesh = mc->compute(grid, 3.0f);
+		
+		AVRMesh mesh;
+		mesh.setName("bloba");
+		mesh.addTextCoord(AVR::vec2{0.0f, 0.0f});
+		for(uint i{0}; i<mcmesh.verts.size(); i++) {
+			float3 normal{mcmesh.normals[i]};
+			mesh.addNormal(AVR::vec3{normal.x, normal.y, normal.z});
+			
+			float3 vert{mcmesh.verts[i]};
+			mesh.addVertex(AVR::vec3{vert.x, vert.y, vert.z});
+		}
+		for(uint i{0}; i<mcmesh.verts.size(); i+=3) {
+			mesh.addFace(AVRFace{{i, i+1, i+2}, {i, i+1, i+2}, {0, 0, 0}});
+		}
+		
+		AVRMaterial material;
+		material.setName("red");
+		material.setDiffuse(0.5f, 0.0f, 0.0f);
+		material.setSpecular(0.9f, 0.9f, 0.9f);
+		material.setAmbient(0.0f, 0.0f, 0.0f);
+		material.setEmissive(1.0f, 0.0f, 0.0f);
+		material.setOpacity(1.0f);
+		material.setSpecularLevel(0.25f);
+		material.setGlossiness(0.15f);
+		
+		mesh.setMaterialId(0);
+		
+		AVRFile file;
+		file.addMaterial(material);
+		file.addMesh(&mesh);
+		
+		file.save("out.avr");
+		
 	} catch ( cl::Error &e ) {
 		cerr
 		  << "OpenCL runtime error at function " << endl
