2229ea3b05ce5798872b9ccd9334f463e6530ce0
Miłosz Kosobucki <milosz@kosobucki.pl>
2013-04-18 23:18:58 +0900

    Ported SimpleBlob test to new exporter

--- tests/01_simpleBlob.cpp
+++ tests/01_simpleBlob.cpp
@@ -3,6 +3,7 @@
 #include "grid.h"
 #include "blob.h"
 #include "marchingcubes.h"
+#include "exporters.h"
 #include "util.h"
 
 #include <iostream>
@@ -35,37 +36,8 @@ int main()
 		MarchingCubes* mc = ctx.getMcProgram();
 		MCMesh mcmesh = mc->compute(grid, 3.0f);
 		
-		AVRMesh mesh;
-		mesh.setName("bloba");
-		mesh.addTextCoord(AVR::vec2{0.0f, 0.0f});
-		for(uint i{0}; i<mcmesh.verts.size(); i++) {
-			float3 normal{mcmesh.normals[i]};
-			mesh.addNormal(AVR::vec3{normal.x, normal.y, normal.z});
-			
-			float3 vert{mcmesh.verts[i]};
-			mesh.addVertex(AVR::vec3{vert.x, vert.y, vert.z});
-		}
-		for(uint i{0}; i<mcmesh.verts.size(); i+=3) {
-			mesh.addFace(AVRFace{{i, i+1, i+2}, {i, i+1, i+2}, {0, 0, 0}});
-		}
-		
-		AVRMaterial material;
-		material.setName("red");
-		material.setDiffuse(0.5f, 0.0f, 0.0f);
-		material.setSpecular(0.9f, 0.9f, 0.9f);
-		material.setAmbient(0.0f, 0.0f, 0.0f);
-		material.setEmissive(1.0f, 0.0f, 0.0f);
-		material.setOpacity(1.0f);
-		material.setSpecularLevel(0.25f);
-		material.setGlossiness(0.15f);
-		
-		mesh.setMaterialId(0);
-		
-		AVRFile file;
-		file.addMaterial(material);
-		file.addMesh(&mesh);
-		
-		file.save("out.avr");
+		vector<MCMesh*> meshes = {&mcmesh};
+		export_avr(meshes, "out.avr");
 		
 	} catch ( cl::Error &e ) {
 		cerr
