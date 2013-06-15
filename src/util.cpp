MiKom/karstgen

commit c3b636f7c26532e8960fb9bf31bd8a3666fa3ec9

Partially fix computation of multiple GPUs

src/util.cpp
@@ -225,12 +225,6 @@ void run1DKernelMultipleQueues(
 	const std::vector<cl::Event>* events)
 {
 	unsigned int devSize = globalSize / queues.size();
-	if(localSize > 0) {
-		globalSize = roundUp(localSize, globalSize);
-		uint groupsPerQueue = (globalSize / localSize) / queues.size();
-		devSize = groupsPerQueue * localSize;
-	}
-	vector<cl::Event> waitEvents;
 	for(int i=0; i < queues.size(); i++) {
 		const cl::CommandQueue& queue = queues[i];
 		cl::Event event;

commit 7c27f1d7781e904eecdfee3a5f4d0b185ebce586

Fixed compilation error introduced in c3b636f

src/util.cpp
@@ -225,6 +225,7 @@ void run1DKernelMultipleQueues(
 	const std::vector<cl::Event>* events)
 {
 	unsigned int devSize = globalSize / queues.size();
+	vector<cl::Event> waitEvents;
 	for(int i=0; i < queues.size(); i++) {
 		const cl::CommandQueue& queue = queues[i];
 		cl::Event event;